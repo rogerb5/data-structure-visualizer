8a38c52565ca620f97655180aae5ed1a92dc2533
Logan Stoltz <lstoltz@ewu.edu>
2023-11-21 00:32:34 -0800

    Working on Button Functionality

--- js/structures/stackvisualizer.js
+++ js/structures/stackvisualizer.js
@@ -1,70 +1,95 @@
 console.log('stackvisualizer code output test');
 
-
-/******* On-Click Events *******/
-
-
-
-
 /******* Stack Data strucutre *******/
-class Stack {
-    constructor() {
-        this.values = [];
-        this.size = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const values = [];
+    const size = 0;
+
+    const stackContainer = document.getElementById('stack-container');
+
+    function updateStack() {
+      while (stackContainer.firstChild) {
+        stackContainer.removeChild(stackContainer.firstChild);
+      }
+
+      for (let i = size - 1; i >= 0; i--) {
+        const stackBox = document.createElement('div');
+        stackBox.className = 'stack-box';
+        stackBox.textContent = values[i];
+        stackContainer.appendChild(stackBox);
+      }
     }
+    
+    function stackIsEmpty() {
+        if(size < 0 ) 
+            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0!");
 
-    isEmpty() {
-        if(this.size < 0 ) 
-            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0");
-
-        else if(this.size == 0)
-            return true;
+        else if(size === 0) {
+            alert(`The stack is empty.`);
+        }
         else
-            return false;
+            alert(`The stack is NOT empty.`);
     }
 
-    push(num) {
-        this.values.push(num);
-        this.size++;
+    function stackPush() {
+        const value = parseInt(document.getElementById('push-input').value, 10);
+        alert(`Pushing ${value} to the Stack`);
+        values.push(value);
+        size++;
+        updateStack();
     }
 
-    pop() {
-        if(this.size < 0 ) 
-            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0");
+    function stackPop() {
+        if(size < 0 ) 
+            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0!");
 
-        else if(this.size = 0) {
-            return null
+        else if(size === 0) {
+            alert('Stack is empty!');
         }
         else {
-
-            this.size--;
-            return this.values.pop();
+            values.pop;
+            size--;
+            updateStack();
         }
     }
 
-    getSize() {
-        if(this.size < 0 )
-            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0");
-
-        return this.size;
+    function stackGetSize() {
+        if(size < 0 )
+            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0!");
+        
+        alert(`Stack size: ${size}`);
     }
 
-    peek() {
-        if(this.size < 0 )
-            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0");
+    function stackPeek() {
+        if(size < 0 )
+            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0!");
 
-        else if(this.isEmpty()) {
-            return null;
+        else if(!(size === 0)) {
+            alert(`Top of the stack: ${values[size - 1]}`);
         }
-        return this.values[this.size - 1];
-    }
-
-    clear() {
-        if(this.size < 0 )
-            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0");
-
-        while (this.size > 0) {
-            this.pop();
+        else {
+            alert(`Stack is empty!`);
         }
+        
     }
-}
+
+    function stackClear() {
+        if(size < 0 )
+            throw new Error("InvalidArgumentExcpetion - size cannot be less than 0!");
+
+        while (size > 0) {
+            values.stackPop();
+        }
+        alert(`Stack is now empty!`);
+    }
+
+    /******* On-Click Events *******/
+
+    document.getElementById('Push-Button').addEventListener('click', stackPush);
+    document.getElementById('Pop-Button').addEventListener('click', stackPop);
+    document.getElementById('Peek-Button').addEventListener('click', stackPeek);
+    document.getElementById('isEmpty-Button').addEventListener('click', stackIsEmpty);
+    document.getElementById('size-Button').addEventListener('click', stackGetSize);
+    document.getElementById('Clear-Button').addEventListener('click', stackClear);
+});
