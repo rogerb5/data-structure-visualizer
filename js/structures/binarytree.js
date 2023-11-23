fb17a8f8951253f6d3a7760fdacd3af008d50070
Evelyn Barragan <ebarragan5@ewu.edu>
2023-11-23 11:42:53 -0800

    insertion

--- js/structures/binarytree.js
+++ js/structures/binarytree.js
@@ -49,25 +49,30 @@ class BinarySearchTree extends NodeBST {
     }
 
     insertionNode(node, newNode) {
-        if (this.compare(newNode.data, node.data) === Comparison.SMALLER) { //if the new node is smaller that the current node
-            if (node.nodeLeft === null) { //checks to see if left of the node is empty
-                node.nodeLeft = newNode; //insert new node on the left if smaller than current node
-                newNode.nodeParent = node; //setting the parent of the new node
+        const comparisonResult = this.compare(newNode.data, node.data);
+    
+        if (comparisonResult === Comparison.SMALLER) {
+            if (node.nodeLeft === null) {
+                node.nodeLeft = newNode;
+                newNode.nodeParent = node;
+            } else {
+                this.insertionNode(node.nodeLeft, newNode);
             }
-            else {
-                this.insertionNode(node.nodeLeft, newNode); //traverse the left subtree 
+        } else if (comparisonResult === Comparison.GREATER) {
+            if (node.nodeRight === null) {
+                node.nodeRight = newNode;
+                newNode.nodeParent = node;
+            } else {
+                this.insertionNode(node.nodeRight, newNode);
             }
-        }
-        else if (this.compare(newNode.data, node.data) === Comparison.GREATER) { //if the new node is greater than the current node 
-            if (node.nodeRight === null) { //checks to see if right of the node is empty 
-                node.nodeRight = newNode; //insert new node on the right 
-                newNode.nodeParent = node; //setting the parent of the new node 
-            }
-            else {
-                this.insertionNode(node.nodeRight, newNode); //traverse the right subtree 
-            }
+        } else {
+            // Handle the case when newNode.data is equal to node.data.
+            // You can choose to ignore, update, or handle it based on your requirements.
+            // For now, we'll ignore equal values.
+            console.log("Ignoring equal values:", newNode.data);
         }
     }
+    
 
     Remove(data) {
         this.root = this.removeNode(this.root, data);
