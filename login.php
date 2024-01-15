9a7f34927f74a10eb2a390a8c4d25338fb152afe
Evebarr20 <Ebarragan5@ewu.edu>
2024-01-14 16:03:18 -0800

    login issue pt.10

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ session_start();
 
 $servername = "localhost";
 $username = "root";
-$password = "SeniorProject7"; // Change this to your actual database password
-$dbname = "mydb"; // Change this to your actual database name
+$password = "SeniorProject7";
+$dbname = "mydb";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,36 +12,32 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Debugging: Check what data is received
-var_dump($_POST);
+if (!isset($_POST["email"], $_POST["Passwrd"])) {
+    exit("Please fill out the required fields.");
+}
 
-if (isset($_POST["email"], $_POST["Passwrd"])) {
-    $email = $_POST["email"];
-    $password = $_POST["Passwrd"];
+$email = $_POST["email"];
+$password = $_POST["Passwrd"];
 
-    $stmt = $conn->prepare("SELECT user_name, Passwrd FROM registration WHERE email = ?");
-    $stmt->bind_param('s', $email);
-    $stmt->execute();
-    $stmt->bind_result($user_name, $hashed_password);
-    $stmt->fetch();
+$stmt = $conn->prepare("SELECT user_name, Passwrd FROM registration WHERE email = ?");
+$stmt->bind_param('s', $email);
+$stmt->execute();
+$stmt->bind_result($user_name, $hashed_password);
+$stmt->fetch();
 
-    // After this line: $stmt->fetch();
+// debug: print values
+echo "User Name: $user_name<br>";
+echo "Entered Password: $password<br>";
+echo "Stored Hash: $hashed_password<br>";
+
 if (password_verify($password, $hashed_password)) {
-    echo "Password verification successful. Welcome, $user_name!";
-    // You can set session variables or redirect to the home page here
+    echo "Password verification successful";
 } else {
-    echo "Entered Password: $password<br>";
-    echo "Stored Hash: $hashed_password<br>";
     echo "Incorrect email or password.";
 }
 
-    $stmt->close();
-} else {
-    echo "Please fill out the required fields.";
-}
-
+$stmt->close();
 $conn->close();
 ?>
 
-?>
 
