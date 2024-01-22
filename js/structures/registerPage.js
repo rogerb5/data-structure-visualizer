339880ed733a57961178cf8fc8379e6dd20b63e2
Logan Stoltz <lstoltz@ewu.edu>
2024-01-21 21:25:42 -0800

    .

--- js/structures/registerPage.js
+++ js/structures/registerPage.js
@@ -3,11 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDdVbR-4agnruN2XLkZCrJsfu9ywxw6UgQ",
   authDomain: "data-structures-learning-tool.firebaseapp.com",
