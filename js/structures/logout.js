3ec642c1270f3525f65d3f64ff76a1832b2269fe
Evelyn Barragan <ebarragan5@ewu.edu>
2024-01-23 18:12:56 -0800

    backend checktable to work

--- js/structures/logout.js
+++ js/structures/logout.js
@@ -13,6 +13,8 @@ firebase.initializeApp(firebaseConfig);
 //intialize variables
 const auth = firebase.auth();
 
+const database = firebase.database();
+
 // Logout function
 function logout() {
     auth.signOut().then(function() {
@@ -25,3 +27,39 @@ function logout() {
         alert("Error during logout: " + error.message);
     });
 }
+
+// Check if the user is authenticated
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in, get the user ID
+      var userId = user.uid;
+
+      // Create a reference to the database with the user ID
+      var database1 = firebase.database().ref('users/' + userId);
+
+      // Read data on page load
+      database1.once('value').then(function(snapshot) {
+        document.getElementById('bst-checkbox').checked = snapshot.val()?.bst || false;
+        document.getElementById('doubly-checkbox').checked = snapshot.val()?.doubly || false;
+        document.getElementById('maxheap-checkbox').checked = snapshot.val()?.maxheap || false;
+        document.getElementById('stack-checkbox').checked = snapshot.val()?.stack || false;
+      });
+
+      // Update Checkbox Handling
+      document.getElementById('bst-checkbox').addEventListener('change', function() {
+        database1.update({ bst: this.checked });
+      });
+
+      document.getElementById('doubly-checkbox').addEventListener('change', function() {
+        database1.update({ doubly: this.checked });
+      });
+
+      document.getElementById('maxheap-checkbox').addEventListener('change', function() {
+        database1.update({ maxheap: this.checked });
+      });
+
+      document.getElementById('stack-checkbox').addEventListener('change', function() {
+        database1.update({ stack: this.checked });
+      });
+    }
+  });
