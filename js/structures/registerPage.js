bee44322b59895f20c8517791c20e05b0457c219
Logan Stoltz <lstoltz@ewu.edu>
2024-01-26 19:25:21 -0800

    .

--- js/structures/registerPage.js
+++ js/structures/registerPage.js
@@ -1,38 +1,62 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdVbR-4agnruN2XLkZCrJsfu9ywxw6UgQ",
-  authDomain: "data-structures-learning-tool.firebaseapp.com",
-  databaseURL: "https://data-structures-learning-tool-default-rtdb.firebaseio.com",
-  projectId: "data-structures-learning-tool",
-  storageBucket: "data-structures-learning-tool.appspot.com",
-  messagingSenderId: "455972295533",
-  appId: "1:455972295533:web:9d0b9946e62ada9a488064",
-  measurementId: "G-ZDM444BB2T"
-};
+    apiKey: "AIzaSyBs8bh4xiBuPm3YBLqVGNId3Y8wiptMRHY",
+    authDomain: "seniorproject07-fa9c6.firebaseapp.com",
+    projectId: "seniorproject07-fa9c6",
+    storageBucket: "seniorproject07-fa9c6.appspot.com",
+    messagingSenderId: "605296004206",
+    appId: "1:605296004206:web:82cc68752684059b846a7f",
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
-const auth = getAuth(app);
+const database = firebase.database();
+const auth = firebase.auth();
 
-document.getElementById('Reg').addEventListener('click', (e) => {
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+function register() {
+  // Get all our input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+  fullname = document.getElementById('fullname').value
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      alert('User has been created');
-      // Redirect logic here
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-});
+  // Validate input fields
+  if (!validate_email(email) || !validate_password(password)) {
+    alert('Email or Password is not valid!')
+    return
+    // Don't continue running the code
+  }
+  if (!validate_field(fullname)) {
+    alert('Name is not valid!')
+    return
+  }
+ 
+
+  auth.createUserWithEmailAndPassword(email, password)
+  .then(function(){
+    // Declare user variable
+    var user = userCredential.user;
+    // Add this user to Firebase Database
+    var database_ref = database.ref();
+    // Create User data
+    var user_data = {
+      email: email,
+      fullname: fullname,
+      last_login: Date.now()
+    };
+
+    // Push to Firebase Database
+    database_ref.child('users/' + user.uid).set(user_data);
+    alert('User Created!!');
+  })
+  .catch((error) => {
+    // Firebase will use this to alert of its errors
+    const error_code = error.code;
+    const error_message = error.message;
+
+    alert(error_message);
+  });
+}
