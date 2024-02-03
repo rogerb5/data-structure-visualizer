86c0e2150d04bbb75d178771f23a7be39b38e0a3
Logan Stoltz <lstoltz@ewu.edu>
2024-02-03 13:16:49 -0800

    Bugs: peek and code buttons

--- js/structures/stackvisualizer.js
+++ js/structures/stackvisualizer.js
@@ -5,16 +5,20 @@ console.log('stackvisualizer code output test');
 function openMoreInfo(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
+    var selectedTab = document.getElementById(tabName);
+
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
+    selectedTab.style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
+
+
 
 /******* Stack Data strucutre *******/
 document.addEventListener('DOMContentLoaded', function () {
